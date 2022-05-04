cc376845c9d58804222992b7e9fe502944ef3fa0
RengeHrpk <ramen1taro@gmail.com>
2022-05-05 03:01:53 +0900

    GameManager add
    未完成

--- MaidFallGame/Assets/Scripts/GameManager.cs
+++ MaidFallGame/Assets/Scripts/GameManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public static GameManager GetInstance()
+    {
+        if (GameObject.FindWithTag("GameManager") == null)
+        {
+            // ここに生成の呪文を追加
+        }
+
+        return instance;
     }
 }
