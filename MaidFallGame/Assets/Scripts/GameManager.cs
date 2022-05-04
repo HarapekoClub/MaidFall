d589b9a7f4b797c73a6b5b57c0f2d59d8cd0df3b
RengeHrpk <ramen1taro@gmail.com>
2022-05-05 03:37:11 +0900

    GameManager一応(Resourcesは後で消す)

--- MaidFallGame/Assets/Scripts/GameManager.cs
+++ MaidFallGame/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,13 +18,30 @@ public class GameManager : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// ゲームマネージャーのゲッター
+    /// </summary>
+    /// <returns></returns>
     public static GameManager GetInstance()
     {
-        if (GameObject.FindWithTag("GameManager") == null)
+        if (!GameObject.FindGameObjectWithTag("GameManager"))
         {
-            // ここに生成の呪文を追加
+            Debug.Log("GameManagerを生成します");
+            instance = Instantiate(Resources.Load<GameObject>("Prefabs/GameManager")).GetComponent<GameManager>();
         }
 
         return instance;
     }
+
+    /// <summary>
+    /// シーン遷移
+    /// </summary>
+    /// <param name="sceneName">遷移先のシーン名</param>
+    public void JumpScene(string sceneName)
+    {
+        Debug.Log("シーン" + sceneName + "に移動します");
+        SceneManager.LoadScene(sceneName);
+    }
+
+
 }
